d494dd517ce0a58631ec1f4b28998bd3240b2faa
Shivanshu Singh <shivnshus@gmail.com>
2018-12-08 16:52:03 +0530

    Add ETS cache support

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -7,7 +7,8 @@ defmodule KV.Registry do
   Starts the registry.
   """
   def start_link(opts) do
-    GenServer.start_link(__MODULE__, :ok, opts)
+    server = Keyword.fetch!(opts, :name)
+    GenServer.start_link(__MODULE__, server, opts)
   end
 
   @doc """
@@ -15,14 +16,18 @@ defmodule KV.Registry do
   Returns `{:ok, pid}` if the bucket exists, `:error` otherwise
   """
   def lookup(server, name) do
-    GenServer.call(server, {:lookup, name})
+    case :ets.lookup(server, name) do
+      [{^name, pid}] -> {:ok, pid}
+      [] -> :error
+    end
+    # GenServer.call(server, {:lookup, name})
   end
 
   @doc """
   Ensures there us a bucket associated with the given `name` in `server`
   """
   def create(server, name) do
-    GenServer.cast(server, {:create, name})
+    GenServer.call(server, {:create, name})
   end
 
   @doc """
@@ -34,32 +39,44 @@ defmodule KV.Registry do
 
   ## Server Callbacks
 
-  def init(:ok) do
-    names = %{}
+  def init(table) do
+    # names = %{}
+    names = :ets.new(table, [:named_table, read_concurrency: true])
     refs = %{}
     {:ok, {names, refs}}
   end
 
-  def handle_call({:lookup, name}, _from, {names, _} = state) do
-    {:reply, Map.fetch(names, name), state}
-  end
+  # def handle_call({:lookup, name}, _from, {names, _} = state) do
+  #   {:reply, Map.fetch(names, name), state}
+  # end
 
-  def handle_cast({:create, name}, {names, refs}) do
-    if Map.has_key?(names, name) do
-      {:noreply, {names, refs}}
-    else
-      # {:ok, pid} = KV.Bucket.start_link([])
-      {:ok, pid} = DynamicSupervisor.start_child(KV.BucketSupervisor, KV.Bucket)
-      ref = Process.monitor(pid)
-      refs = Map.put(refs, ref, name)
-      names = Map.put(names, name, pid)
-      {:noreply, {names, refs}}
+  def handle_call({:create, name}, _from, {names, refs}) do
+    case lookup(names, name) do
+      {:ok, pid} ->
+        {:reply, pid, {names, refs}}
+      :error ->
+        {:ok, pid} = DynamicSupervisor.start_child(KV.BucketSupervisor, KV.Bucket)
+        ref = Process.monitor(pid)
+        refs = Map.put(refs, ref, name)
+        :ets.insert(names, {name, pid})
+        {:reply, pid, {names, refs}}
     end
+    # if Map.has_key?(names, name) do
+    #   {:noreply, {names, refs}}
+    # else
+    #   # {:ok, pid} = KV.Bucket.start_link([])
+    #   {:ok, pid} = DynamicSupervisor.start_child(KV.BucketSupervisor, KV.Bucket)
+    #   ref = Process.monitor(pid)
+    #   refs = Map.put(refs, ref, name)
+    #   names = Map.put(names, name, pid)
+    #   {:noreply, {names, refs}}
+    # end
   end
 
   def handle_info({:DOWN, ref, :process, _pid, _reason}, {names, refs}) do
     {name, refs} = Map.pop(refs, ref)
-    names = Map.delete(names, name)
+    # names = Map.delete(names, name)
+    :ets.delete(names, name)
     {:noreply, {names, refs}}
   end
 
--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -1,9 +1,9 @@
 defmodule KV.RegistryTest do
   use ExUnit.Case, async: true
 
-  setup do
-    registry = start_supervised!(KV.Registry)
-    %{registry: registry}
+  setup context do
+    _ = start_supervised!({KV.Registry, name: context.test})
+    %{registry: context.test}
   end
 
   test "spawns buckets", %{registry: registry} do
@@ -20,6 +20,7 @@ defmodule KV.RegistryTest do
     KV.Registry.create(registry, "shopping")
     {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
     Agent.stop(bucket)
+    _ = KV.Registry.create(registry, "bogus")
     assert KV.Registry.lookup(registry, "shopping") == :error
   end
 
@@ -28,7 +29,16 @@ defmodule KV.RegistryTest do
     {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
 
     Agent.stop(bucket, :shutdown)
+    _ = KV.Registry.create(registry, "bogus")
     assert KV.Registry.lookup(registry, "shopping") == :error
+  end
+
+  test "bucket can crash at any time", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+
+    Agent.stop(bucket, :shutdown)
+    catch_exit KV.Bucket.put(bucket, "milk", 3)
   end
 
 end
